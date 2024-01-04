800fa28955886fc4eefe7f17ae28f10a27ac9db3
ruanm <247987314@qq.com>
2024-01-04 13:53:39 +0800

    fix: Add chat page

--- client/src/router/index.jsx
+++ client/src/router/index.jsx
@@ -1,12 +1,13 @@
 import { useContext } from "react";
 import RegisterAndLoginForm from "../pages/RegisterAndLoginForm";
 import { UserContext } from "../context/userContext";
+import Chat from "../pages/Chat";
 
 const Routes = () => {
   const { username, id } = useContext(UserContext);
 
   if (username) {
-    return `logged in! ${username}`;
+    return <Chat />;
   }
 
   return <RegisterAndLoginForm />;
